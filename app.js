cfa8e652d8ee34b821b5544a54a0ddf97d986f7f
Duy Dao <jimmydao@gmail.com>
2021-12-18 12:29:13 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@email.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    }
+});
+
+app.mount('#app');
+
+//Original code (In the code block above, we're moving the code from the app.js file to the index.html file)
+// const app = Vue.createApp({
+//         template: '<h1>Hello {{firstName}}</h1>',
+//         data() {
+//             return {
+//                 firstName: 'John',
+//             }
+//         }
+//     });
+    
+// app.mount('#app');
